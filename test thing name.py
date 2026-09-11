0546a1e04dc7d1d463a2f5f4665206649c06bce3
aureliaemery <aureliaemery@seattleacademy.org>
2026-09-11 18:50:22 +0000

    test the turn in sytem

---
+++ test thing name.py
@@ -0,0 +1,14 @@
+name = input ("Enter name: ")
+print (f"Hello {name}!")
+print (f"thats a very pretty name i think {name} is super cool!")
+input()
+print (f"Do you have a pet?")
+answer = input ("Enter yes or no: ")
+if answer == "yes":
+    print(f"What type of pet do you have?")
+    pet = input ("Enter pet type: ")
+    print(f"I have always wanted a {pet} they are so cute!")
+    print(f"Whats their name?")
+    pname = input ("Enter your pets name: ")
+    print 
+
